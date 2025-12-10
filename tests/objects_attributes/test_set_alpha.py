c4623e73b3c614bc825cceac3e6c97d60abd0cb4
MartenPostma <postmam@corderius.nl>
2025-12-10 19:48:13 +0100

    formatted with black

--- tests/objects_attributes/test_set_alpha.py
+++ tests/objects_attributes/test_set_alpha.py
@@ -3,18 +3,19 @@ import pytest
 num_frames = 0
 max_frames = 100
 
-expected = [0, 127,255]
+expected = [0, 127, 255]
 actual = []
+
 
 def test_set_alpha():
     import sys
 
     sys.path.insert(0, ".")
     import play
- 
-    image1 = play.new_image(image='yellow.jpg', size=10, transparency=0)
-    image2 = play.new_image(image='yellow.jpg', size=10, transparency=50)
-    image3 = play.new_image(image='yellow.jpg', size=10, transparency=100)
+
+    image1 = play.new_image(image="yellow.jpg", size=10, transparency=0)
+    image2 = play.new_image(image="yellow.jpg", size=10, transparency=50)
+    image3 = play.new_image(image="yellow.jpg", size=10, transparency=100)
 
     @play.repeat_forever
     def move():
@@ -26,7 +27,7 @@ def test_set_alpha():
             actual = [
                 image1.image.get_alpha(),
                 image2.image.get_alpha(),
-                image3.image.get_alpha()
+                image3.image.get_alpha(),
             ]
             play.stop_program()
 
@@ -37,6 +38,7 @@ def test_set_alpha():
             pytest.fail(
                 f"expected alpha to be {expected_value} but the actual value is {actual_value}"
             )
+
 
 if __name__ == "__main__":
     test_set_alpha()
